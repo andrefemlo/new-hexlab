c9dd013a56a835d4bcffc1b0e6576c91c84f9d6b
André Felipe Melo <andrefemlo@gmail.com>
2024-07-18 15:24:58 -0300

    teste

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
