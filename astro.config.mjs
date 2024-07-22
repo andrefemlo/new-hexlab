ab7df5e28802ff04a2ea28ffbe6fd1c2b2573921
André Felipe Melo <andrefemlo@gmail.com>
2024-07-22 15:56:53 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
-import mdx from "@astrojs/mdx";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hexlab.online/',
-  integrations: [tailwind(), sitemap(), astroImageTools, mdx()],
+  integrations: [tailwind(), sitemap(), astroImageTools, alpinejs()],
   output: "server",
   adapter: vercel()
 });
