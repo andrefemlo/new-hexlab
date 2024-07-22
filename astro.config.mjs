30e91ac2eea1d4f0f9ee9bf28fb343b1c3618d86
André Felipe Melo <andrefemlo@gmail.com>
2024-07-22 16:50:01 -0300

    fixed vercel build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new-hexlab.vercel.app/',
   integrations: [tailwind(), sitemap(), astroImageTools],
-  output: "server",
-  adapter: vercel()
+  base: '/build',
+  output: "static",
+  // adapter: vercel()
 });
---
+++ dist/_astro/hoisted.Ci1-SuZ3.js
@@ -0,0 +1,1 @@
+import"./lenisSmoothScroll.CdeDFJJO.js";import"https://unpkg.com/lenis@1.1.5/dist/lenis.min.js";
