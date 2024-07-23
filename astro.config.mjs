8c7ee4dfb6405c502d63b774d06ab636383ad4b4
André Felipe Melo <andrefemlo@gmail.com>
2024-07-23 10:07:53 -0300

    inline css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import lenis from "astro-lenis";
 
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new-hexlab.vercel.app/',
-  integrations: [tailwind(), sitemap(), astroImageTools, lenis()],
+  integrations: [tailwind(), sitemap(), astroImageTools, lenis(), (await import("@playform/inline")).default()],
   base: '/.',
-  output: "static",
+  output: "static"
   // adapter: vercel()
 });
