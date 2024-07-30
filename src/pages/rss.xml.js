c4b53e63b13f434b4867d0deec6ef12933ce46eb
André Felipe Melo <andrefemlo@gmail.com>
2024-07-30 11:34:11 -0300

    rss posts updated again

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function GET(context) {
   return rss({
     title: 'Hexlab | Blog',
     description: 'Seu blog de tecnologia e criação',
-    site: 'https://new-hexlab.vercel.ap',
+    site: 'https://new-hexlab.vercel.app',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>pt-br</language>`,
   });
