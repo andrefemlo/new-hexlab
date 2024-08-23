74328f4a681f711cdafe62672b65842ac7d3f793
André Felipe Melo <andrefemlo@gmail.com>
2024-08-23 15:08:52 -0300

    testemonies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import lenis from "astro-lenis";
-
 import playformInline from "@playform/inline";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new-hexlab.vercel.app/',
-  integrations: [tailwind(), sitemap(), astroImageTools, lenis(), (await import("@playform/inline")).default()],
+  integrations: [tailwind(), sitemap(), astroImageTools, lenis(), playformInline(), mdx()],
   base: '/.',
   output: "static"
   // adapter: vercel()
--- src/lib/cosmic.js
+++ src/lib/cosmic.js
@@ -1,4 +1,5 @@
 import { createBucketClient } from '@cosmicjs/sdk'
+import axios from 'axios';
 
 const cosmic = createBucketClient({
   bucketSlug: import.meta.env.BUCKET_SLUG,
@@ -10,6 +11,45 @@ export async function getAllPosts() {
     .find({
       type: 'posts'
     })
-    .props('title,slug,metadata.cover_image,created_at')
+    .props('title,slug,metadata,created_at').sort('-created_at').depth(2)
   return data.objects
+}
+export async function getAllCases() {
+  const data = await cosmic.objects
+    .find({
+      type: 'cases'
+    })
+    .props('title,slug,metadata,created_at').sort('-created_at').depth(2)
+  return data.objects
+}
+
+export async function searchContent(query) {
+  if (!query) {
+    return { posts: [], cases: [] };
+  }
+
+  const regex = new RegExp(query, 'i'); // Cria uma expressão regular para busca case-insensitive
+
+  const posts = await cosmic.objects
+    .find({
+      type: 'posts',
+      'metadata.title': { $regex: regex } // Aplica o filtro de busca
+    })
+    .props('title,slug,metadata,created_at')
+    .sort('-created_at')
+    .depth(2);
+
+  const cases = await cosmic.objects
+    .find({
+      type: 'cases',
+      'metadata.title': { $regex: regex } // Aplica o filtro de busca
+    })
+    .props('title,slug,metadata,created_at')
+    .sort('-created_at')
+    .depth(2);
+
+  return {
+    posts: posts.objects || [],
+    cases: cases.objects || []
+  };
 }
